4024931129972fa28f1283aba22bec9861701ff6
Felipe Rocha <felipenrocha19@gmail.com>
2017-06-20 13:54:17 -0300

    Update scoreboard code2.asm

--- scoreboard code2.asm
+++ scoreboard code2.asm
@@ -14,19 +14,20 @@ LOOP: 	beq $s2,$t1,PLACAR1
 	sw $s1,0($s2)
 	addi $s2,$s2,4
 	j LOOP
+	PLACAR1:
 	
-	addi $t0,$zero,-100 #Valor de $t0 = 0
-	addi $t2,$zero,-100 #Valor de $t2 = 0
-	addi $t3,$zero,90 #Valor de $t3 = 90, valor dos segundos
+	addi $t0,$zero,0	 #Valor de $t0 = 0
+	addi $t2,$zero,0	 #Valor de $t2 = 0
+	addi $t3,$zero,20	#Valor de $t3 = 90, valor dos segundos
 	
 #Placar do jogador 1
-PLACAR1:la $a0,PLAYER1   #Endereco da P1
+	la $a0,PLAYER1   #Endereco da P1
 	li $a1,240	#Coluna
 	li $a2,50	#Linha
 	li $a3,0x00A3	#Cores da letra (A3) e fundo(00)
 	li $v0,104	# print string	(do system53)
 	syscall
-#Pontuação do jogador 1
+#PontuaÃ§Ã£o do jogador 1
 VALOR1:	la $a0,($t0)
 	li $a1,240
 	li $a2,60
@@ -41,7 +42,7 @@ PLACAR2:la $a0,PLAYER2  #Endereco da P2
 	li $a3,0x002E	#Cores da letra (2E) e fundo(00)
 	li $v0,104	# print string  (do system53)
 	syscall
-#Pontuação do jogador 2
+#PontuaÃ§Ã£o do jogador 2
 VALOR2: la $a0,($t2)
 	li $a1,240
 	li $a2,90
@@ -67,10 +68,30 @@ Placar_De_Tempo:  la $a0,TIME  #Endereco da TIME
 	syscall
 	
 	#Loop da contagem de segundos
-	TIMER: la $a0,($t3)
+	
+
+	TIMER:
+	
+	la $a0,($t3)
 	li $a1,290
 	li $a2,130
 	li $a3,0XFF
 	li $v0,101	#print int (do system53)
 	syscall
+	addi $a0, $zero, 1000
+	li $v0,32
+	syscall
+	
+	subi $t3, $t3, 1
+	beq $t3,$zero , FIM
+	j TIMER
+
+
+
+
+	FIM: 
+	li $v0, 10
+	syscall
+	
+	
 	
