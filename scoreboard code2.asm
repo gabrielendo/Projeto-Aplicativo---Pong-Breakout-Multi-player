51b3b2fcc195466e7d00af2a4ac7b45c1092ed67
gabrielendo <gabriel-endo@hotmail.com>
2017-06-20 14:29:03 -0300

    Update scoreboard code2.asm

--- scoreboard code2.asm
+++ scoreboard code2.asm
@@ -1,105 +1,128 @@
 .data
 	PLAYER1: .asciiz "Player 1"
 	PLAYER2: .asciiz "Player 2"
-	TIME:    .asciiz "Time"
-	LEFT:	 .asciiz "Left:"
-	ESPAÇOPRETO: .asciiz "  "	#"Limpagem" do contador de segunds
-	
+	TIME: .asciiz "Time"
+	LEFT: .asciiz "Left:"
+	ESPAÇOPRETO: .asciiz "  "	# Reseta do contador de segunds
+	TELAINICIO: .asciiz "telainicio.bin"
 		
 .text
-#Pinta o fundo de preto
+### TELA DE INICIO ###
+# Abre o arquivo
+	la $a0,TELAINICIO
+	li $a1,0
+	li $a2,0
+	li $v0,13
+	syscall
+
+# Le o arquivos para a memoria VGA
+	move $a0,$v0
+	la $a1,0xFF000000
+	li $a2,76800
+	li $v0,14
+	syscall
+
+# Verifica se enter for pressionado
+ENTER:	la $t1,0xFF100000
+	lw $t0,0($t1)
+	andi $t0,$t0,0x0001	# Le bit de Controle Teclado
+  	lw $t2,4($t1)		# Tecla lida
+  	addi $t3, $zero, 10	# Coloca 10 (ascii de enter) em $t3 para a comparacao seguinte
+  	beq $t2, $t3, JOGO	# Se enter for pressionado, vai para jogo
+	j ENTER			# Continua no loop se enter nao for pressionado
+
+# Fecha o arquivo
+JOGO:	move $a0,$s1
+	li $v0,16
+	syscall
+	
+	
+### INICIO DO JOGO ###
+# Preenche o fundo de preto
 	li $t1,0xFF012C00
 	li $s2,0xFF000000
 	li $s1,0x00000000
-LOOP: 	beq $s2,$t1,PLACAR1
+LOOP: 	beq $s2,$t1,PLACAR
 	sw $s1,0($s2)
 	addi $s2,$s2,4
 	j LOOP
-	PLACAR1:
 	
-	addi $t0,$zero,0	 #Valor de $t0 = 0
-	addi $t2,$zero,0	 #Valor de $t2 = 0
-	addi $t3,$zero,90	#Valor de $t3 = 90, valor dos segundos
+PLACAR:	addi $t0,$zero,0	# Valor de $t0 = 0
+	addi $t2,$zero,0	# Valor de $t2 = 0
 	
-#Placar do jogador 1
-	la $a0,PLAYER1   #Endereco da P1
-	li $a1,240	#Coluna
-	li $a2,50	#Linha
-	li $a3,0x00A3	#Cores da letra (A3) e fundo(00)
+# Placar do jogador 1
+	la $a0,PLAYER1	# Endereco da P1
+	li $a1,240	# Coluna
+	li $a2,50	# Linha
+	li $a3,0x00A3	# Cores da letra (A3) e fundo(00)
 	li $v0,104	# print string	(do system53)
 	syscall
-#Pontuação do jogador 1
-VALOR1:	la $a0,($t0)
+# Pontuação do jogador 1
+	la $a0,($t0)
 	li $a1,240
 	li $a2,60
 	li $a3,0x00FF
-	li $v0,101	#print int (do system53)
+	li $v0,101	# print int (do system53)
 	syscall
 	
-#Placar do jogador 2
-PLACAR2:la $a0,PLAYER2  #Endereco da P2
-	li $a1,240	#Coluna
-	li $a2,80	#Linha
-	li $a3,0x002E	#Cores da letra (2E) e fundo(00)
+# Placar do jogador 2
+	la $a0,PLAYER2  # Endereco da P2
+	li $a1,240	# Coluna
+	li $a2,80	# Linha
+	li $a3,0x002E	# Cores da letra (2E) e fundo(00)
 	li $v0,104	# print string  (do system53)
 	syscall
-#Pontuação do jogador 2
-VALOR2: la $a0,($t2)
+# Pontuação do jogador 2
+	la $a0,($t2)
 	li $a1,240
 	li $a2,90
 	li $a3,0X00FF
-	li $v0,101	#print int (do system53)
+	li $v0,101	# print int (do system53)
 	syscall
+		
 	
-	
-	
-	#Placar de tempo restante
-Placar_De_Tempo:  la $a0,TIME  #Endereco da TIME
-	li $a1,240	#Coluna
-	li $a2,120	#Linha
-	li $a3,0xFF	#Cores da letra (2E) e fundo(00)
+# Placar de tempo restante
+	la $a0,TIME	# Endereco da TIME
+	li $a1,240	# Coluna
+	li $a2,120	# Linha
+	li $a3,0xFF	# Cores da letra (2E) e fundo(00)
 	li $v0,104	# print string  (do system53)
 	syscall
 	
-	la $a0,LEFT  #Endereco da TIME
-	li $a1,240	#Coluna
-	li $a2,130	#Linha
-	li $a3,0xFF	#Cores da letra (2E) e fundo(00)
+	la $a0,LEFT	# Endereco da TIME
+	li $a1,240	# Coluna
+	li $a2,130	# Linha
+	li $a3,0xFF	# Cores da letra (2E) e fundo(00)
 	li $v0,104	# print string  (do system53)
 	syscall
 	
-	#Loop da contagem de segundos
-	
+#Loop da contagem de segundos
 
-	TIMER:
-	#Limpagem de contador
-	la $a0, ESPAÇOPRETO
+addi $s3,$zero,15	# Valor de $s3 = 90, valor dos segundos
+
+TIMER:	la $a0, ESPAÇOPRETO	# Reseta de contador
 	li $a1,290
 	li $a2,130
 	li $a3,0X00
 	li $v0,104
 	syscall
 	
-	
-	la $a0,($t3)
+	la $a0,($s3)		# Imprime o tempo
 	li $a1,290
 	li $a2,130
 	li $a3,0XFF
-	li $v0,101	#print int (do system53)
+	li $v0,101		# print int (do system53)
 	syscall
-	addi $a0, $zero, 1000
+	
+	addi $a0, $zero, 1000	# Sleep por 1s
 	li $v0,32
 	syscall
 	
-	subi $t3, $t3, 1
-	beq $t3,-1 , FIM
+	subi $s3, $s3, 1	# Decrementa o tempo em 1
+	beq $s3,-1 , FIM
 	j TIMER
-
-
-
-
-	FIM: 
-	li $v0, 10
-	syscall
 	
+	
+FIM: 	li $v0, 10
+	syscall
 	
